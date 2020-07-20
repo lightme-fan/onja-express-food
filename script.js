31d9544ebdd94bb5608e0886967960532e710696
Fanilo Andriamarohaja <fanilo.and@onja.org>
2020-07-20 09:57:24 +0100

    not finished yet

--- script.js
+++ script.js
@@ -8,101 +8,81 @@ const body = document.querySelector('body');
 
 // Event listener to show the form
 const clickAddOrder = (event) => {
+    modalInner.innerHTML = `
+        <form>
+            <p>Your name :</p>
+            <input
+            class="input-form"
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Enter your name here"
+            required
+            />
+            <p>Please select your dish :</p>
+            <select name="dish" class="select-form" required>
+            <option value="romazava">Romazava</option>
+            <option value="koba">Koba</option>
+            <option value="ravitoto">Ravitoto</option>
+            <option value="mokary">Mokary</option>
+            <option value="achard">Achard</option>
+            <option value="masikita">Masikita</option>
+            <option value="sambos">Sambos</option>
+            <option value="mofo-baolina">Mofo baolina</option>
+            <option value="ranonapango">Ranonapango</option>
+            </select>
+            <p>Please select the size of your plate :</p>
+            <input type="radio" id="small" name="size" value="small" required />
+            <label for="small">Small</label><br />
+            <input type="radio" id="medium" name="size" value="medium" />
+            <label for="medium">Medium</label><br />
+            <input type="radio" id="large" name="size" value="large" />
+            <label for="large">Large</label><br />
+            <p>How many pieces do you want to order?</p>
+            <input
+            class="input-form"
+            type="number"
+            id="quantity"
+            name="amount"
+            placeholder="Enter a number here"
+            required
+            />
+            <button class="submitOrder" type="submit">Add this order</button>
+        </form>
+    `;
     modalOuter.classList.add('open');
 }
-
-const handleClick = (event) => {
-    const isOutside = !event.target.closest('.inner');
-    if (isOutside) {
-        modalOuter.classList.remove('open');
-    }
-};
-
-const escapeClick = (event) => {
-    if (event.key === 'Escape') {
-        modalOuter.classList.remove('open');
-    }
-};
-
 addOrderButton.addEventListener('click', clickAddOrder);
-window.addEventListener('keydown', escapeClick);
-modalOuter.addEventListener('click', handleClick);
 
-const form = document.querySelector('form');
-const name = document.querySelector('#name');
-const selectDish = document.querySelector('.select-form');
-const radioButton = document.querySelectorAll(`[type="radio"]`);
-const quantity = document.querySelector('#quantity');
-const submitOrderButton = document.querySelector(`[type="submit"]`);
-const detailButton = document.querySelector('.detail');
-const deleteButton = document.querySelector('.served');
+// handling all buttons
+const handleClickButton = (event) => {
 
-// Submit form
-const submitOrder = (event) => {
-    event.preventDefault();
-    
-    const newOrder = `
+    // Submit button of the form 
+    if (event.target.matches('button.submitOrder')) {
+        const name = document.querySelector('#name');
+        const newOrder = `
         <div class="order">
             <span class="title">${name.value}</span>
             <button class="details">Details</button>
             <button class="served">Delete order</button>
         </div>
     `;
-    orderList.insertAdjacentHTML("beforeend", newOrder);
+        event.target = newOrder;
+        orderList.insertAdjacentHTML('afterbegin', newOrder);
+        modalOuter.classList.remove('open');
+    }
 
-    modalOuter.classList.remove('open');
-
-    form.reset();
-}
-submitOrderButton.addEventListener('click', submitOrder);
-
-
-const outerDetails = document.querySelector('outer-details');
-const innerDetails = document.querySelector('inner-details');
-
-// Showing details
-const showDetail = (event) => {
-    // if (event.target.classList.contains('details')) {
+    // Detail button 
+    if (event.target.classList.matches('button.details')) {
         
-        innerDetails.innerHTML = `
-            <div>
-                <h2>${name.value}</h2>
-                <h3>Order:</h3>
-                <h4>${quantity.value} ${radioButton.value} ${selectDish.value}</h4>
-                <img max-width="100%" max-height="100%" src="https://picsum.photos/400" alt>
-            </div
-        `;
-        // body.insertAdjacentHTML('beforeend', showingDetails);
-        outerDetails.classList.add('open');
-
-    //     const detailsOfOrder = event.target;
-    //     detailsOfOrder.add('.outer-detail');
-    // }
-}
-
-const handleDetails = (event) => {
-    const isOutside = !event.target.closest('.inner');
-    if (isOutside) {
-        outerDetails.classList.remove('open');
     }
-};
 
-const escapeDetails = (event) => {
-    if (event.key === 'Escape') {
-        outerDetails.classList.remove('open');
-    }
-};
-window.addEventListener('keydown', escapeDetails);
-// outerDetails.addEventListener('click', handleDetails);
-// detailButton.addEventListener('click', showDetail);
-
-
-// Delete list
-const deleteFunction = (event) => {
+    // Delete button
     if (event.target.classList.contains('served')) {
         const order = event.target;
         order.parentElement.remove();
-    }   
+    }
+
 }
-document.addEventListener('click', deleteFunction);
+window.addEventListener('click',handleClickButton);
 
